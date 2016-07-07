fc39e2ee942bfcf288dd9187a95ec466268d4471
vapva <volodymyr.panchenko@gmail.com>
2016-07-07 15:39:09 +0300

    7

--- src/test/java/testFirst/AllTests.java
+++ src/test/java/testFirst/AllTests.java
@@ -3,10 +3,9 @@ package testFirst;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-import testFirst.*;
 
 @RunWith(Suite.class)
-@SuiteClasses({TestStart.class, 
+@SuiteClasses({
 	TestInputHelper.class,
 	TestWaterParam.class})
 public class AllTests {
