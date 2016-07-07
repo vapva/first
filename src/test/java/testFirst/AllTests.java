ebe3394eaefcfb2f7cf3b2c36824c018b30cffc3
vapva <volodymyr.panchenko@gmail.com>
2016-07-07 15:37:10 +0300

    6

--- src/test/java/testFirst/AllTests.java
+++ src/test/java/testFirst/AllTests.java
@@ -3,6 +3,7 @@ package testFirst;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+import testFirst.*;
 
 @RunWith(Suite.class)
 @SuiteClasses({TestStart.class, 
