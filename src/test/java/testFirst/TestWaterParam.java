b4da0711f17ba3dfa3615015093836c4448b04d2
vapva <volodymyr.panchenko@gmail.com>
2016-07-07 15:35:08 +0300

    5

--- src/test/java/testFirst/TestWaterParam.java
+++ src/test/java/testFirst/TestWaterParam.java
@@ -2,6 +2,7 @@ package testFirst;
 
 import static org.junit.Assert.*;
 
+import java.sql.Date;
 import java.util.Calendar;
 
 import main.Start;
@@ -17,19 +18,46 @@ public class TestWaterParam {
 		long i = 8;
 		Calendar cd =Calendar.getInstance();
 		cd.set(2015, 0, 2);
-		System.out.println(cd.getTime());
 		WaterParam wp=new WaterParam(i,
 				new java.sql.Date(cd.getTime().getTime()),
 				"COBAS", 
 				16.2d);
-		System.out.println("1. " +Start.getWaterParamByID(i).toString());
-		System.out.println("2. " +wp);
 		assertEquals(Start.getWaterParamByID(i).equals(wp), true);
 	}
 	
 	@Test
 	public void testEqualsObject() {
-		fail("Not yet implemented");
+		Calendar cd =Calendar.getInstance();
+		cd.set(2015, 0, 2);
+		WaterParam wp1Param = new WaterParam(1, new Date(cd.getTime().getTime()), 
+				"COBAS", Double.valueOf("16.2"));
+		cd.set(2015, 0, 2);
+		WaterParam wp2Param = new WaterParam(1, new Date(cd.getTime().getTime()), 
+				"COBAS", Double.valueOf("16.2"));
+		assertEquals(wp1Param, wp2Param);
+	}
+	
+	@Test
+	public void testInqualsObject1() {
+		Calendar cd =Calendar.getInstance();
+		cd.set(2015, 0, 1);
+		WaterParam wp1Param = new WaterParam(1, new Date(cd.getTime().getTime()), 
+				"COBAS", Double.valueOf("16.2"));
+		cd.set(2015, 0, 2);
+		WaterParam wp2Param = new WaterParam(1, new Date(cd.getTime().getTime()), 
+				"COBAS", Double.valueOf("16.2"));
+		assertFalse(wp1Param.equals(wp2Param));
 	}
 
+	@Test
+	public void testInqualsObject2() {
+		Calendar cd =Calendar.getInstance();
+		cd.set(2015, 0, 1);
+		WaterParam wp1Param = new WaterParam(1, new Date(cd.getTime().getTime()), 
+				"COBAS", Double.valueOf("16.2"));
+		cd.set(2015, 0, 1);
+		WaterParam wp2Param = new WaterParam(1, new Date(cd.getTime().getTime()), 
+				"COBAS", Double.valueOf("16"));
+		assertFalse(wp1Param.equals(wp2Param));
+	}
 }
